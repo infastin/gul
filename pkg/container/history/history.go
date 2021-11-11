d4536364741f4cc325b835643d358e478e9d5867
infastin <thecookied123@gmail.com>
2021-11-11 21:39:32 +0500

    Added history container

---
+++ pkg/container/history/history.go
@@ -0,0 +1,95 @@
+package history
+
+type History struct {
+	index  int
+	defv   interface{}
+	values []interface{}
+}
+
+func Make(defv interface{}) History {
+	return History{
+		index: -1,
+		defv:  defv,
+	}
+}
+
+func New(defv interface{}) *History {
+	return &History{
+		index: -1,
+		defv:  defv,
+	}
+}
+
+func (h *History) Set(val interface{}) {
+	vlen := len(h.values)
+
+	if vlen > 0 && h.index < vlen-1 {
+		h.values = h.values[:h.index+1]
+	}
+
+	h.values = append(h.values, val)
+	h.index++
+}
+
+func (h *History) Back() interface{} {
+	if h.index == -1 {
+		return h.defv
+	}
+
+	h.index--
+	return h.values[h.index]
+}
+
+func (h *History) Forward() interface{} {
+	if h.index >= len(h.values)-1 {
+		return h.defv
+	}
+
+	h.index++
+	return h.values[h.index]
+}
+
+func (h *History) Get() interface{} {
+	if h.index == -1 {
+		return h.defv
+	}
+
+	return h.values[h.index]
+}
+
+func (h *History) Len() int {
+	return len(h.values)
+}
+
+func (h *History) Index() int {
+	return h.index
+}
+
+func (h *History) SetDefault(defv interface{}) {
+	h.defv = defv
+}
+
+func (h *History) Default() interface{} {
+	return h.defv
+}
+
+func (h *History) Reset() {
+	h.index = 0
+}
+
+func (h *History) Clear() {
+	h.index = 0
+	h.values = h.values[:0]
+}
+
+func (h *History) Empty() bool {
+	return len(h.values) == 0
+}
+
+func (h *History) AtBegin() bool {
+	return h.index == -1
+}
+
+func (h *History) AtEnd() bool {
+	return h.index == len(h.values)-1
+}

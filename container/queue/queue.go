02a34798a04fcab63ff2fbb939e22c720f30f6e0
infastin <thecookied123@gmail.com>
2021-11-16 19:21:18 +0500

    Stack and queue

---
+++ container/queue/queue.go
@@ -0,0 +1,73 @@
+package queue
+
+type Element struct {
+	next, prev *Element
+	Value      interface{}
+}
+
+type Queue struct {
+	front, back *Element
+	len         int
+}
+
+func New() *Queue {
+	return &Queue{}
+}
+
+func (q *Queue) Push(value interface{}) *Element {
+	el := &Element{
+		next:  nil,
+		prev:  nil,
+		Value: value,
+	}
+
+	q.len++
+
+	if q.back == nil {
+		q.front = el
+		q.back = el
+	} else {
+		q.back.next = el
+		el.prev = q.back
+		q.back = el
+	}
+
+	return el
+}
+
+func (q *Queue) Pop() *Element {
+	if q.front == nil {
+		return nil
+	}
+
+	q.len--
+	ret := q.front
+
+	q.front = q.front.next
+	if q.front == nil {
+		q.back = nil
+	} else {
+		q.front.prev = nil
+	}
+
+	ret.next = nil
+	ret.prev = nil
+
+	return ret
+}
+
+func (q *Queue) Empty() bool {
+	return q.len == 0
+}
+
+func (q *Queue) Front() *Element {
+	return q.front
+}
+
+func (q *Queue) Back() *Element {
+	return q.back
+}
+
+func (q *Queue) Len() int {
+	return q.len
+}
---
+++ container/stack/stack.go
@@ -0,0 +1,66 @@
+package stack
+
+type Element struct {
+	next, prev *Element
+	Value      interface{}
+}
+
+type Stack struct {
+	top *Element
+	len int
+}
+
+func New() *Stack {
+	return &Stack{}
+}
+
+func (s *Stack) Push(value interface{}) *Element {
+	el := &Element{
+		next:  nil,
+		prev:  nil,
+		Value: value,
+	}
+
+	s.len++
+
+	if s.top == nil {
+		s.top = el
+	} else {
+		s.top.next = el
+		el.prev = s.top
+		s.top = el
+	}
+
+	return el
+}
+
+func (s *Stack) Pop() *Element {
+	if s.top == nil {
+		return nil
+	}
+
+	s.len--
+	ret := s.top
+
+	s.top = s.top.prev
+	if s.top != nil {
+		s.top.next = nil
+	}
+
+	ret.next = nil
+	ret.prev = nil
+
+	return ret
+}
+
+func (s *Stack) Empty() bool {
+	return s.len == 0
+}
+
+func (s *Stack) Top() *Element {
+	return s.top
+}
+
+func (s *Stack) Len() int {
+	return s.len
+}
